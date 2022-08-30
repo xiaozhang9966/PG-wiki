32d0dcdbff4716a6c7e60eef968be0ee238872e5
小张9966 <2387479217@qq.com>
2022-08-30 15:18:55 +0800

    8.30 增加hello接口测试test.http

--- src/main/java/com/jiawa/wiki/controller/TestController.java
+++ src/main/java/com/jiawa/wiki/controller/TestController.java
@@ -1,7 +1,13 @@
 package com.jiawa.wiki.controller;
 
+import org.springframework.core.annotation.MergedAnnotationPredicates;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
 //注解Contorller返回一个
 @RestController
 public class TestController {
@@ -14,8 +20,13 @@ public class TestController {
     @RequestMapping(value = "/hello",method = RequestMethod.GET)
      */
 
-    @RequestMapping("/hello")//http://127.0.0.1:8080/hello 途径
+    @GetMapping("/hello")//http://127.0.0.1:8080/hello 途径
     public String Hello(){
-        return "Hello worle!";
+        return "Hello world!";
+    }
+
+    @PostMapping("/hello/post")
+    public String HelloPost(String name){
+        return "Hello world! post" +name;
     }
 }
