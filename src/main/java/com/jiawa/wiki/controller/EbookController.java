48af019bcc91cf48fdd84afb98d3ca62959ebdd1
小张9966 <2387479217@qq.com>
2022-08-31 17:28:19 +0800

    8.31 封装请求参数和响应参数

--- src/main/java/com/jiawa/wiki/controller/EbookController.java
+++ src/main/java/com/jiawa/wiki/controller/EbookController.java
@@ -1,7 +1,9 @@
 package com.jiawa.wiki.controller;
 
 import com.jiawa.wiki.domain.Ebook;
+import com.jiawa.wiki.req.EbookReq;
 import com.jiawa.wiki.resp.CommonResp;
+import com.jiawa.wiki.resp.EbookResp;
 import com.jiawa.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,9 +22,9 @@ public class EbookController {
 
 
     @GetMapping("/list")//支持所有类型 返回List集合 类型为->Ebook自己写的domain 实体类->数据库里的字段 return 调用注入你写的名字ebookService的list方法
-    public CommonResp list(){
-        CommonResp<List<Ebook>> resp = new CommonResp<>();//new Com 返回的参数类型是List集合的Ebook这个实体类的各种数据类
-        List<Ebook> list = ebookService.list();//调用ebookServerice的List方法 返回给list这个变量 类型是List集合的Ebook
+    public CommonResp list(EbookReq req){
+        CommonResp<List<EbookResp>> resp = new CommonResp<>();//new Com 返回的参数类型是List集合的Ebook这个实体类的各种数据类
+        List<EbookResp> list = ebookService.list(req);//调用ebookServerice的List方法 返回给list这个变量 类型是List集合的Ebook
         resp.setContent(list);//get方法的set 把数据库的参数设置进来，类型是自定义的List<Ebook>
         return resp;//返回resp
 
--- src/main/java/com/jiawa/wiki/service/EbookService.java
+++ src/main/java/com/jiawa/wiki/service/EbookService.java
@@ -1,19 +1,36 @@
 package com.jiawa.wiki.service;
 
+import com.fasterxml.jackson.databind.util.BeanUtil;
 import com.jiawa.wiki.domain.Ebook;
+import com.jiawa.wiki.domain.EbookExample;
 import com.jiawa.wiki.mapper.EbookMapper;
+import com.jiawa.wiki.req.EbookReq;
+import com.jiawa.wiki.resp.EbookResp;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 
 public class EbookService {
-   @Resource
+    @Resource
     private EbookMapper ebookMapper;
 
-    public List<Ebook> list(){
-    return ebookMapper.selectByExample(null);
+    public List<EbookResp> list(EbookReq req) {
+        EbookExample ebookExample = new EbookExample();//domain下的example mybatis给我门自动生成很多方法了new 处理
+        EbookExample.Criteria criteria = ebookExample.createCriteria();//当作where语句  把criter创建出来
+        criteria.andNameLike("%" + req.getName() + "%");
+        List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);//selectByExample = 基本查询语句 括号里面就是条件 我们写了where语句下的模糊查询 Like
+
+        List<EbookResp> respList = new ArrayList<>();
+        for (Ebook ebook : ebookList) {
+            EbookResp ebookResp = new EbookResp();//把响应的类型new出来，才能使用
+            BeanUtils.copyProperties(ebook, ebookResp);//要拷贝哪个  到拷贝到哪里去
+            respList.add(ebookResp);
+        }
+        return respList;
     }
 }
