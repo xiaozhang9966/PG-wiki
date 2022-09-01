b31bbe97042002ff0ad7a52d2bd5c4a3cdbc8ec8
小张9966 <2387479217@qq.com>
2022-09-01 15:45:55 +0800

    9.1 集成Ant Desgin Vue

---
+++ web/src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+        
+      <div :style="{ background: 'rgb(190, 200, 200)', padding: '26px 16px 16px' }">
+        <a-button ghost>Default</a-button>
+      </div>
+
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+
+export default defineComponent({
+  name: 'Home',
+  components: {
+    HelloWorld,
+  },
+});
+</script>
