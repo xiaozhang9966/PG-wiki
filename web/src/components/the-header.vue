e1c96cb82ea168e7b3b9bdc63065e87bf785c648
小张9966 <2387479217@qq.com>
2022-09-04 14:57:09 +0800

    9.4 电子书的管理页面修改

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -7,7 +7,7 @@
         v-model:selectedKeys="selectedKeys1"
         :style="{ lineHeight: '64px' }"
     >
-      <a-menu-item key="Home"><router-link to="/">首页</router-link></a-menu-item>
+      <a-menu-item key="/"><router-link to="/">首页</router-link></a-menu-item>
       <a-menu-item key="About"><router-link to="/admin/ebook">电子书管理</router-link></a-menu-item>
       <a-menu-item key="AdminEbook"><router-link to="about">关于我们</router-link></a-menu-item>
     </a-menu>
